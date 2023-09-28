bf08175b6e2872039ce0726541c96af73be43896
Rafael Checchia Moreno <rafaelchecchia@proton.me>
2023-09-28 09:50:02 -0300

    add Connection files

---
+++ util/config.php
@@ -0,0 +1,7 @@
+<?php
+
+define("DB_HOST", "mysql-server");
+define("DB_NAME", "crud_tenis");
+define("DB_USER", "newuser");
+define("DB_PASSWORD", "password");
+define("BASE_URL", "/html/crud_tenis");
