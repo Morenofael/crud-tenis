b1fbd06383ec76ccbb2f3468397ebdef8222cb57
Rafael Checchia Moreno <rafaelchecchia@proton.me>
2023-09-28 11:42:09 -0300

    adicionar connection/php

--- view/tenis/form.php
+++ view/tenis/form.php
@@ -1,6 +1,8 @@
+<?php
 include_once(__DIR__ . "/../include/header.php");
+?>
 <h2><?php echo (!$aluno || $aluno->getId() <= 0 ? 'Inserir' : 'Alterar') ?> Aluno</h2>
 
 <form id="frmTenis" method="POST">
-    
+
 </form>
