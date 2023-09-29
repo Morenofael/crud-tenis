8df50fac3e63cad160f3e9d88e34c2d5efd5e7dc
Rafael Checchia Moreno <rafaelchecchia@proton.me>
2023-09-28 21:09:05 -0300

    finalizar EsporteDAO

---
+++ dao/EsporteDAO.php
@@ -0,0 +1,29 @@
+<?php
+
+include_once(__DIR__ . "/../util/Connection.php");
+include_once(__DIR__ . "/../model/Esporte.php");
+
+class EsporteDAO{
+    private $conn;
+
+    public function __construct()
+    {
+        $this->conn = Connection::getConnection();
+    }
+    public function list() {
+        $sql = "SELECT * FROM esportes";
+        $stm = $this->conn->prepare($sql);
+        $stm->execute();
+        $result = $stm->fetchAll();
+        return $this->mapBancoParaObjeto($result);
+    }
+    private function mapBancoParaObjeto($result) {
+        $esportes = array();
+        foreach($result as $reg) {
+            $e = new Esporte();
+            $e->setId($reg['id'])
+                ->setNome($reg['nome']);
+            array_push($esportes, $e);
+        }
+}
+}
