b5e408a7879e6362c56a90dad584ee0f0271fd5e
Rafael Checchia Moreno <rafaelchecchia@proton.me>
2023-09-28 23:01:44 -0300

    implementar TenisService

---
+++ service/TenisService.php
@@ -0,0 +1,37 @@
+<?php 
+
+require_once(__DIR__ . "/../model/Tenis.php");
+
+class TenisService {
+
+    public function validarDados(Tenis $tenis) {
+        $erros = array();
+        
+        if(! $tenis->getNome()) {
+            array_push($erros, "Informe o nome!");
+        }
+
+        if(! $tenis->getTamanho()) {
+            array_push($erros, "Informe o tamanho!");
+        }
+
+        if(! $tenis->getPreco()) {
+            array_push($erros, "Informe o pre,co!");
+        }
+
+        if(! $tenis->getMarca()) {
+            array_push($erros, "Informe a marca!");
+        }
+
+        if(! $tenis->getSexo()) {
+            array_push($erros, "Informe o sexo!");
+        }
+        
+        if(! $tenis->getEsporte()) {
+            array_push($erros, "Informe o esporte!");
+        }
+
+        return $erros;
+    }
+
+}
